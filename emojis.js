862360fbb04b696c59adcb98b281099ce6edf190
Nils K <git@nkn-it.de>
2020-08-15 15:07:38 +0200

    init

---
+++ emojis.js
@@ -0,0 +1,42 @@
+// Emojis
+const emojiName = {
+    happyFace: '😀',
+    partyingFace: '🥳',
+    faceWithTearsOfJoy: '😂',
+    smilingFaceWithHearts: '🥰',
+    nerdFace: '🤓',
+    astonishedFace: '😲',
+    unamusedFace: '😒',
+    sleepingFace: '😴',
+    heart: '🧡',
+    thumbsUp: '👍',
+    thumbsDown: '👎',
+    fire: '🔥',
+    smilingPoop: '💩'
+};
+
+// Channel name
+const urlString = window.location.search;
+console.log("URL Params: " + urlString);
+const urlParams = new URLSearchParams(urlString);
+const channel = urlParams.get('c')
+const defaultChannel = '3feWGFFm';
+const channelName = channel ? channel : defaultChannel;
+
+// MQTT over WebSockets
+const mqttServer = "broker.mqttdashboard.com";
+const mqttPort   = 8000;
+// MQTT topic
+const mqttTopic = 'emojireaction/' + channelName + '/reactions';
+const mqttClientId = "emojireaction" + channelName + Math.floor(Math.random() * 10000) + 10;  //clientId
+
+// Create a new Client object with your broker's hostname, port and your own clientId
+// http://www.eclipse.org/paho/files/jsdoc/Paho.MQTT.Client.html
+var client = new Paho.Client(
+    mqttServer,
+    Number(mqttPort),
+    mqttClientId
+);
+console.log("MQTT server: " + mqttServer + ':' + mqttPort);
+console.log("Topic: " + mqttTopic);
+console.log("Client ID: " + mqttClientId);
